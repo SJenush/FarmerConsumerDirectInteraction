dacb10fd044fadd810e2309b2150aba3467e12c3
Jenush <jenushsjiffin@gmail.com>
2024-09-21 11:37:46 +0530

    Php script added to add products without more validation

--- AddProducts.php
+++ AddProducts.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+$msg="";
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  $prod_name=$_POST['prod_name'];
+  $prod_des=$_POST['prod_des'];
+  $prod_quantity=$_POST['prod_quantity'];
+  $unit=$_POST['unit'];
+  $isCount=TRUE;
+  if($unit=="kg"){
+    $isCount=FALSE;
+  }
+  $farmer_name=$_SESSION['Username'];
+  $img_name=$_FILES['prod_img']['name'];
+  $img_tmp=$_FILES['prod_img']['tmp_name'];
+  $fileExtension = pathinfo($img_name, PATHINFO_EXTENSION);
+  
+  $img_un_name='img'.uniqid().'.'. $fileExtension;
+  $img_path='static/userimg/'. $img_un_name;
+  //Setting connection
+  $con=mysqli_connect("localhost","root","","dbfarmerconsumer");
+  if(!$con){
+    die("Error while connection".mysqli_connect_error());
+  }
+  if(move_uploaded_file($img_tmp,$img_path)){
+  $sql="INSERT INTO products (ProductName	,ProductDescription,ProductQuantity,isCount,ProductImgPath,FarmerName) VALUES('$prod_name','$prod_des','$prod_quantity','$isCount','$img_path','$farmer_name')";
+  if(mysqli_query($con,$sql)){
+    $msg= "Product Successfully added";
+  }else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+    }
+  }else{
+    $msg= "Could not upload image!";
+  }
+mysqli_close($con);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,30 +68,31 @@
 </nav>
 <div class="contain">
 <center>
+      <h2 class="msg"><?php echo $msg?></h2>
        <img src="/static/img/farmer.png" class="ico"> 
        <h2>Farmer Name</h2></center>
 <div class="flex-container">
   <div>
-    <form class="form">
+    <form class="form" action="" method="POST" enctype="multipart/form-data">
        
         <h3 class="na">Add Product:</h3>
         <table>
             <tr class="rw">
-                <td>Product Name:</td><td><input type="text" class="form-control"></td>
+                <td>Product Name:</td><td><input type="text" class="form-control" name="prod_name"></td>
             </tr>
             <tr class="rw">
-                <td>Avaliable quantity:</td><td><input type="number"class="form-control"></td>
+                <td>Avaliable quantity:</td><td><input type="number"class="form-control" name="prod_quantity"></td>
             </tr>
             <tr class="rw">
                 <td>Quantity unit:</td><td><input type="radio"class="form-check-input" value="kg" name="unit" checked><label for="" class="form-check-label lab">in kg</label>
                 <input type="radio"class="form-check-input" value="count" name="unit"><label class="form-check-label lab">count</label></td>
             </tr>
             <tr class="rw">
-                <td>Description:</td><td><textarea type="text"class="form-control"></textarea></td>
+                <td>Description:</td><td><textarea type="text"class="form-control" name="prod_des"></textarea></td>
             </tr>
             <tr class="rw">
                 <td>Add To Image:</td>
-                <td><input type="file" class="form-control"></td>
+                <td><input type="file" class="form-control" name="prod_img"></td>
                 
             </tr>
             <tr class="rw">
