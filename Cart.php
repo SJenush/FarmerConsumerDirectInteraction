8133dbe49cd3b890e585fcc2eea307be98ca0c6c
Jenush <jenushsjiffin@gmail.com>
2024-09-23 19:44:36 +0530

    Cart.php added with functionality

---
+++ Cart.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Products</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="static/css/nav.css">
+    <link rel="stylesheet" href="static/css/viewProduct_style.css">
+    <link rel="icon" href="/static/img/mainicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary cu_nav">
+  <div class="container-fluid">
+    <div>
+      <img src="static/img/leaves.png" alt="" class="main_icon">
+    <a class="navbar-brand cu_white cu_Ti anton-regular  " href="#">GreenHarvest </a>
+  </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav c_ter">
+      <script src="https://cdn.lordicon.com/lordicon.js"></script>
+      <div class="div_ico">
+        <img src="/static/img/home.png" class="img_ico">
+        <a class="nav-link  c_top" aria-current="page" href="index.php" >Home</a>
+      </div>
+      <div class="div_ico">
+        <img src="/static/img/add-product.png" class="img_ico c_add">
+        <a class="nav-link c_top" href="AddProducts.php">Add product</a></div>
+        <div class="div_ico">
+          <img src="/static/img/search.png" class="img_ico">
+        <a class="nav-link c_top" href="#">View product</a></div>
+        <div class="div_ico">
+          <img src="/static/img/logout.png" class="img_ico">
+        <a class="nav-link  c_top" href="logout.php">Logout</a></div>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <h2 class="topic">Cart Items:</h2>
+  <?php 
+  session_start();
+  if(isset($_SESSION['Username'])){
+  $user=$_SESSION['Username'];
+  }else{
+    header("Location:login.php");
+  }
+  $con=mysqli_connect("localhost","root","","dbfarmerconsumer");
+  if(!$con)
+  {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  $sql="SELECT * FROM cart WHERE Username='$user' ORDER by CartDate DESC;";
+  $result=mysqli_query($con,$sql);
+  if($result&&mysqli_num_rows($result)>0){
+      
+  }else{
+      echo "<div class='d-flex justify-content-center align-items-center' style='height: 200px; color:green;'>
+    <h2>Cart is empty!</h2>
+    </div>";
+  }
+  mysqli_close($con);
+  $x=0;
+  foreach($result as $row):?>
+    <?php if($x%4==0):?>
+      <div class="card-group">
+      <?php endif;?>
+  <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+  <div class="card cu_card">
+    <img src="<?php echo $row['ProductImgPath']?>" class="card-img-top cu_img" alt="...">
+    <div class="card-body">
+    <span class="p_cost paytone-one-regular">₹<?php echo $row['ProductCost'];?></span>
+      <h5 class="card-title paytone-one-regular"><?php echo $row['ProductName'];?></h5>
+      <p class="card-text poppins-medium">Farmer Name: <?php echo $row['FarmerName'];?></p>
+      <p class="card-text poppins-medium">Available Quantity:<?php echo $row['ProductQuantity']; if($row['isCount']){echo " count";}else{echo " kg";}?></p>
+      <button class="btn btn-darkgreen poppins-medium" data-bs-toggle="modal" data-bs-target="#exampleModal">Buy Now</button>
+    </div>
+  </div>
+  </div>
+  <?php if($x%4==3):?>
+    </div>
+      <?php endif;?>
+    <?php $x++;?>
+  <?php endforeach;?>
+</div>
+</body>
+</html>
