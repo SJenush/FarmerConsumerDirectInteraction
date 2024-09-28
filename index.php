64a918d3e603de1c39d4d04903382dd7d2156240
Jen <Jen@gmail.com>
2024-09-28 12:07:48 +0530

    navbar added to index.php

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<p>
 <?php 
     session_start();
     if(isset($_SESSION['Username'])){
-        echo $_SESSION['Username'];
+        //echo $_SESSION['Username'];
     }else{
         //echo $_SESSION['Username'];
         header("Location:login.php");
     }
 ?>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="static/css/nav.css">
+    <link rel="stylesheet" href="static/css/viewProduct_style.css">
+    <link rel="icon" href="/static/img/mainicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include("nav_bar.php")?>
+</body>
+</html>
